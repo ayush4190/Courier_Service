5f25468c04a294544fa5d351b8c98f48df738c1f
mickey4190 <a.ayushkumar1997@gmail.com>
2019-10-20 18:49:07 +0530

    basic layout

--- README.md
+++
@@ -1,2 +0,0 @@
-# viewTube 
-This project aims at making an video streaming web application allows you to download the videos that you stream .
---
+++ connection.php
@@ -0,0 +1,15 @@
+<?php
+
+$conn=mysqli_connect("localhost","root","ayush4190","speed_age");
+
+if(!$conn)
+{
+	echo 'could not connect'.mysqli_error();
+}
+
+
+
+
+
+
+?>
